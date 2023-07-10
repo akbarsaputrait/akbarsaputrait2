d1ddba350607cf5cce422a7715d0480d88164625
akbarsaputrait <akbarsaputrait@outlook.com>
2023-07-10 16:20:33 +0700

    improve seo

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,22 @@
-"use client";
 import DetailBlog from "@components/contents/blog/DetailBlog";
+import { defaultMeta } from "@helpers/meta.helper";
+import { capitalize, join, split } from "lodash";
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    slug: string | null | undefined;
+  };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    ...defaultMeta,
+    title: `${capitalize(
+      join(split(params.slug || "", "-"), " ")
+    )} - @akbarsaputrait`,
+  };
+}
 
 export default function Slug() {
   return <DetailBlog />;
---
+++ src/library/helpers/meta.helper.ts
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+
+export const defaultMeta: Metadata = {
+  title: "Akbar Anung Yudha Saputra",
+  description: "Personal website of Akbar Anung Yudha Saputra",
+  abstract: "Personal website of Akbar Anung Yudha Saputra",
+  authors: {
+    name: "Akbar Anung Yudha Saputra",
+    url: "http://akbarsaputrait.github.io/",
+  },
+  keywords: ["akbar", "anung", "yudha", "saputra", "akbar anung yudha saputra", "akbarsaputrait"],
+  robots: "index, follow",
+  appleWebApp: {
+    title: "Akbar Anung Yudha Saputra",
+  },
+  openGraph: {
+    type: "website",
+    countryName: "Indonesia",
+    siteName: "Akbar Anung Yudha Saputra",
+    title: "Akbar Anung Yudha Saputra",
+    url: "http://akbarsaputrait.github.io/",
+  },
+};
